cb14552f61d8f26728de25daa7743b349d7f0488
Mikiejoe <omoshjoe02@gmail.com>
2023-12-23 18:22:12 +0300

    change the api doc ui to use redoc

--- oppsproject/urls.py
+++ oppsproject/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/account/', include('dj_rest_auth.urls')),
     path('api/v1/account/register/', include('dj_rest_auth.registration.urls')),
-    path('api/v1/docs/', schema_view.with_ui('swagger', cache_timeout=0),
+    path('api/v1/docs/', schema_view.with_ui('redoc', cache_timeout=0),
          name='schema-swagger-ui'),
     path('api/v1/proposal/', include('proposals.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
