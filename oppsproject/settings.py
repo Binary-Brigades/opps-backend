4b5472d65c36105dda03ae811eab045af74ccf4b
Mikiejoe <omoshjoe02@gmail.com>
2023-12-23 14:51:47 +0300

    collect static

--- oppsproject/settings.py
+++ oppsproject/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import dj_database_url
 from decouple import config
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -152,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
